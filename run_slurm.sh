fb8d83ddbecb663334da6b9f68de25e9455920ea
ljansen <lukas.jansen@iee.fraunhofer.de>
2025-07-08 16:23:28 +0200

    misc: update gitignore and run_slurm.sh for cluster usage

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,3 +1,8 @@
 # SLURM specifications made in default.cluster.yaml & the individual rules
 #snakemake --cluster-config config/config.cluster.yaml --cluster "sbatch -p {cluster.partition} -t {cluster.walltime} -o {cluster.output} -e {cluster.error} -c {threads} --mem {resources.mem_mb}" --jobs 199 --latency-wait 60 --keep-going --configfile config/config.yaml
-snakemake solve_sector_networks --cluster-config configs/cluster_config.yaml --cluster "sbatch -p {cluster.partition} -t {cluster.walltime} -c {cluster.cpus_per_task} --mem {cluster.mem_mb} -x {cluster.exclude}" --jobs 199 --latency-wait 60 --keep-going
+snakemake solve_sector_networks --cluster-config configs/cluster_config.yaml \
+--cluster "sbatch -p {cluster.partition} -t {cluster.walltime} -c {cluster.cpus_per_task} --mem {cluster.mem_mb} -x {cluster.exclude}" \
+--jobs 199 --latency-wait 60 --keep-going \
+--configfile config.RE_classes_EG_2050.yaml
+--rerun-trigger mtime
+#--until prepare_network \
