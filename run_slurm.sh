9808c90eb656909b76567b730a7e880c308223f0
ljansen <lukas.jansen@iee.fraunhofer.de>
2025-07-22 01:28:31 +0200

    runslurm

--- run_slurm.sh
+++ run_slurm.sh
@@ -3,7 +3,7 @@
 snakemake solve_sector_networks --cluster-config configs/cluster_config.yaml \
 --cluster "sbatch -p {cluster.partition} -t {cluster.walltime} -c {cluster.cpus_per_task} --mem {cluster.mem_mb} -x {cluster.exclude}" \
 --jobs 199 --latency-wait 60 --keep-going \
---configfile config.yaml \
-# --until prepare_network \
-# --rerun-trigger mtime \
-cp configs/scenarios_iee/config.DKS_CL_2030.yaml results/DKS_CL_2030/configs/
+--configfile configs/DKS/config.DKS_MA_2030.yaml \
+--rerun-trigger mtime 
+#--until prepare_network \
+# cp configs/DKS/config.DKS_MA_2030.yaml results/DKS_MA_2030/configs/
